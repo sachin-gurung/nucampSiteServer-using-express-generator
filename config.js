03cee18977f6c235caaeebd9b8e401cff7b06b94
Sachin Gurung <saching061@hotmail.com>
2021-04-29 07:27:25 -0700

    Temporarily removed config.js file to add later

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
-    'facebook': {
-        clientId: '1625355167654261',
-        clientSecret: '29af92b5def0a7b0e5c2e25a09d0bd54'
-    }
-}
